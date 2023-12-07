9420e5d8eb5c62ed2f21d5aa5593cd8aa6fe8562
raffay <raffaysyedd@gmail.com>
2023-12-07 16:40:51 -0500

    Fixed for day 1 part a

--- python/day1/day1_code.py
+++ python/day1/day1_code.py
@@ -1,29 +1,30 @@
-def find_number(inputList : list[str]) -> list[str]:
+def find_all_numbers(input: str) -> str:
+    output = ''
+    for char in input:
+        if char.isdigit():
+            output += char 
+    return output
+
+def generate_full_number_list(inputList: list[str]) -> list[int] :
     outputList = []
     for input in inputList:
-        output = ""
-        for char in input:
-            if char.isdigit():
-                output += char
-            if(len(output) == 2):
-                break
-
-        if(len(output) < 2):
-            output +=output
-        outputList.append(output)
-    return outputList 
-    
-
-
+        output = find_all_numbers(input)
+        if len(output) > 1 :
+            outputList.append(int(output[0]+output[len(output)-1]))
+        else:
+            outputList.append(int(output+ output))
+    return outputList
+        
+def find_sum_part_a():
+    inputList = parse_input('input.txt')
+    outputList = generate_full_number_list(inputList)
+    return sum(outputList)
 
 def parse_input(filename: str) -> list[str]:
     with open(filename) as f:
         return [line.strip() for line in f.readlines()]
 
-
-
 if __name__ == '__main__':
-    # print(parse_input('example.txt'))
-    print(find_number(parse_input('example.txt')))
+    print(f'sum : {find_sum_part_a()}')
 
 
